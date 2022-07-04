5ba3ddfe1abaa4681a8fea5eee05f887c5f76087
Nikita Jain <106953552+20niki@users.noreply.github.com>
2022-07-04 16:22:43 +0530

    Add files via upload

---
+++ Pa3.java
@@ -0,0 +1,26 @@
+class Pa3
+{
+    public static void main(String[] args)
+    {
+        int i,j;
+
+        for (i = 65; i <= 70; i++)
+        {
+            
+            for ( j = 65; j <= i; j++)
+            {
+                System.out.print((char)+ j+ " ");
+            }
+            System.out.println();
+        }
+        for ( i = 70; i >= 65; i--)
+        {
+             
+            for ( j = 65; j <= i - 1; j++)
+            {
+                System.out.print((char)+ j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pa8.java
@@ -0,0 +1,18 @@
+class Pa8
+{
+    public static void main(String[] args)
+    {
+        int i,j;
+
+        for (i = 70; i >= 65; i--)
+        {
+            
+            for ( j = 70; j >=i; j--)
+            {
+                System.out.print((char)+ j+ " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
